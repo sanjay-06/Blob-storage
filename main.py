6d5cb79c90c8d8b06f7fe7c12318a6a910d5ccbf
sanjay <san692001@gmail.com>
2022-01-27 13:05:39 +0530

    added static files for frontend

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from routes.user import user
 from routes.navigate import navigator
 from routes.handlefile import filerouter
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
+
+app.mount("/static", StaticFiles(directory="html/static"), name="static")
 
 app.include_router(user)
 

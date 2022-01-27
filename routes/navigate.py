a7c8366640dbb31a264021798113f5cbde01518b
sanjay <san692001@gmail.com>
2022-01-27 11:37:40 +0530

    Session management

--- routes/navigate.py
+++ routes/navigate.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter,Request
+from models.Session import cookie,backend
+from models.Basicverifier import verifier
+from fastapi import APIRouter, Depends,Request,HTTPException,status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-
-
 navigator=APIRouter()
 templates=Jinja2Templates(directory="html")
 
@@ -10,15 +10,31 @@ templates=Jinja2Templates(directory="html")
 def write_home(request : Request):
     return templates.TemplateResponse("index.html",{"request":request})
 
-@navigator.get("/files",response_class=HTMLResponse)
+# async def get_token(token:str = Depends(oauthobj.get_token())):
+#     try:
+#         payload=jwt.decode(token,oauth.get_jwtsecret(),algorithms=['HS256'])
+#         user = payload.email
+#         print(payload)
+
+#     except:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="invalid email"
+#             )
+
+#     return user
+
+
+@navigator.get("/files",response_class=HTMLResponse,dependencies=[Depends(cookie)])
+# def write_home(request : Request = Depends(get_token)):
 def write_home(request : Request):
     return templates.TemplateResponse("files.html",{"request":request})
 
-@navigator.get("/permission",response_class=HTMLResponse)
+@navigator.get("/permission",response_class=HTMLResponse,dependencies=[Depends(cookie)])
 def write_home(request : Request):
     return templates.TemplateResponse("permission.html",{"request":request})
 
-@navigator.get("/upload",response_class=HTMLResponse)
+@navigator.get("/upload",response_class=HTMLResponse,dependencies=[Depends(cookie)])
 def write_home(request : Request):
     return templates.TemplateResponse("upload.html",{"request":request})
 
--- routes/user.py
+++ routes/user.py
@@ -1,22 +1,40 @@
 import jwt
-from fastapi import APIRouter, Depends,Form
+from uuid import UUID
+from fastapi import APIRouter, Depends,Form, Response
 from models.OAuth2 import oauth
+from models.Session import SessionData
 from models.user import User
 from config.db import collection
 from schemas.user import userEntity,usersEntity
 from models.hasher import Hasher
 from models.OAuth2 import oauth
 from bson import objectid
+from uuid import uuid4
+from models.Session import cookie,backend
+from models.Basicverifier import verifier
 
 user=APIRouter()
+
+# oauthobj = oauth()
 
 @user.get('/database/file')
 async def find_all_users():
     print(usersEntity(collection.find()))
     return usersEntity(collection.find())
 
+@user.get("/logout")
+async def del_session(response: Response, session_id: UUID = Depends(cookie)):
+    print(session_id)
+    await backend.delete(session_id)
+    cookie.delete_from_response(response)
+    return "deleted session"
+
+@user.get("/whoami", dependencies=[Depends(cookie)])
+async def whoami(session_data: SessionData = Depends(verifier)):
+    return session_data
+
 @user.post("/login")
-async def login(email:str = Form(...),password:str = Form(...)):
+async def login(response:Response,email:str = Form(...),password:str = Form(...)):
      queryresult=collection.find_one({"email":email})
      if queryresult == None:
           return {"message":"user not found"}
@@ -28,12 +46,20 @@ async def login(email:str = Form(...),password:str = Form(...)):
      user_obj=User.get_userobj(email=email,password=password)
      token=jwt.encode(user_obj,oauth.get_jwtsecret())
 
+     session = uuid4()
+     print(session)
+     data = SessionData(username=token)
+     await backend.create(session, data)
+     cookie.attach_to_response(response, session)
+
+    #  oauthobj.set_token(token)
+
      return {"access_token":token,"token_type":"bearer"}
 
 
-@user.post('/')
-async def index(token:str=Depends(oauth.get_oauthscheme())):
-    return {'the_token':token}
+# @user.post('/')
+# async def index(token:str=Depends(oauth.get_oauthscheme())):
+#     return {'the_token':token}
 
 
 @user.post('/register')
@@ -49,3 +75,4 @@ async def update_user(id,email:str= Form(...),password:str=Form(...)):
         "$set":dict(User.get_userobj(email=email,password=password))
     })
     return userEntity(collection.find_one({"_id":objectid(id)}))
+

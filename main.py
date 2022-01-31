52cd496a7c1eab8922cdd67abca8568ec99b945c
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2022-01-31 19:09:41 +0530

    port config

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routes.user import user
 from routes.navigate import navigator
 from routes.handlefile import filerouter
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app=FastAPI()
 
@@ -13,3 +14,5 @@ app.include_router(user)
 app.include_router(navigator)
 
 app.include_router(filerouter)
+
+uvicorn.run(app,host='0.0.0.0',port=8000)

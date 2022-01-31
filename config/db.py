2c38ea2b2c59d91bbafb72bb1220f8e1683608af
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2022-01-31 18:14:12 +0530

    configured .env

--- config/db.py
+++ config/db.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 mongo_url=os.getenv("Mongo_Database_URL")
 
-conn = MongoClient("mongodb+srv://sanjay:1234@cluster0.gxmvm.mongodb.net/cloudwiry?retryWrites=true&w=majority")
+conn = MongoClient(mongo_url)
 
 db= conn.get_database('cloudwiry')
 

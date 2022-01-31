f8d008e4a81bda0c8cf265f0b8f5f02b7951b99a
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2022-01-31 18:13:31 +0530

    configured env

--- config/db.py
+++ config/db.py
@@ -1,4 +1,10 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+mongo_url=os.getenv("Mongo_Database_URL")
 
 conn = MongoClient("mongodb+srv://sanjay:1234@cluster0.gxmvm.mongodb.net/cloudwiry?retryWrites=true&w=majority")
 
@@ -7,4 +13,3 @@ db= conn.get_database('cloudwiry')
 collection = db.usercredentials
 
 permission = db.permissions
-
